101892c6c01892bcf99858f9a0f6ac16daec0230
burk <lukas@quantenbrot.de>
2016-11-15 17:11:08 +0100

    More speical codes added

--- experiments.R
+++ experiments.R
@@ -2,13 +2,14 @@
 
 library(stringr) # String manipulation
 library(rvest)   # For HTML magic
-library(dplyr)
+library(dplyr)   # %>% and other magic
+library(tidyr)   # For extended cleanup I'm to lazy to implement in base R
 
 
-##### src: http://www.kbv.de/html/85.php
+# src: http://www.kbv.de/html/85.php
 # "EBM zur Offlineverwendung" -> Download einer zip-Datei
 
-
+#### Regular EBM files (one html file per EBM, usually) ####
 ebm_files <- list.files("EBMBrowserHtml/ebm/html/", pattern = "^\\d{3}.*", full.names = T)
 
 ebm_tabelle <- plyr::ldply(ebm_files, function(file) {
@@ -39,18 +40,44 @@ ebm_tabelle <- plyr::ldply(ebm_files, function(file) {
                   return(tbl)
                 }, .progress = "text")
 
-# Get old EBMs for completion's sake
+#### Get old EBMs for completion's sake ####
 
-ebm_old <- read_html("EBMBrowserHtml/ebm/html/4_162394112750509016313156.html")
+ebm_old <- read_html("EBMBrowserHtml/ebm/html/4_162394112750509016313156.html", encoding = "ISO-8859-1")
 ebm_old <- html_table(ebm_old, fill = TRUE)
 ebm_old <- ebm_old[[3]]
 ebm_old <- ebm_old[-1, c(1, 2)]
 ebm_old <- ebm_old[ebm_old[1] != "", ]
 names(ebm_old) <- names(ebm_tabelle)[c(1, 2)] 
 
-# Union them
+#### "Nicht gesondert berechnungsfähige ..." ####
 
-ebm_tabelle_full         <- dplyr::bind_rows(ebm_tabelle, ebm_old)
+ebm_special <- read_html("EBMBrowserHtml/ebm/html/1_162398017933962904420416.html", encoding = "ISO-8859-1")
+ebm_special <- html_table(ebm_special, fill = TRUE)
+ebm_special <- ebm_special[[3]]
+ebm_special <- ebm_special[ebm_special$X1 != "", c("X1", "X2")]
+ebm_special <- ebm_special[-1, ] 
+names(ebm_special) <- c("ebmcode", "ebmlabel")
+
+# Strip text from ebmcode, convert to , or handle separately
+
+ebm_special$ebmcode <- str_replace_all(ebm_special$ebmcode, pattern = "Aus ", replacement = "")
+ebm_special$ebmcode <- str_replace_all(ebm_special$ebmcode, pattern = "/", replacement = ",")
+ebm_special         <- tidyr::separate_rows(ebm_special, ebmcode, sep = ", ")
+
+#### Union individual tables and write ####
+
+# ebm_tabelle_full         <- dplyr::bind_rows(ebm_tabelle, ebm_old, ebm_special)
+
+# Bind explicitly only previously unmatched ebmcodes
+ebm_tabelle_full <- bind_rows(ebm_tabelle, 
+                              filter(ebm_old, !(ebm_old$ebmcode %in% ebm_tabelle$ebmcode)))
+ebm_tabelle_full <- bind_rows(ebm_tabelle_full,
+                              filter(ebm_special, !(ebm_special$ebmcode %in% ebm_tabelle_full$ebmcode)))
+                              
+# Guess NA price from value below to easily identify duplicate rows
+ebm_tabelle_full         <- tidyr::fill(ebm_tabelle_full, price, .direction = "up")
+# Remove duplicate rows
+ebm_tabelle_full         <- dplyr::distinct(ebm_tabelle_full)
 ebm_tabelle_full$ebmcode <- as.character(ebm_tabelle_full$ebmcode)
 
 # Write table to disk
