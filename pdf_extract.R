1e12d2e5c009c3b6ef703cfc55548d0a06fcde4e
burk <lukas@quantenbrot.de>
2016-11-15 17:11:20 +0100

    Putting stuff in a function

--- pdf_extract.R
+++ pdf_extract.R
@@ -5,16 +5,25 @@ ebm <- read_lines("EBM_Gesamt___Stand_4._Quartal_2016.txt", locale = locale(enco
 # stringi::stri_enc_detect(ebm)
 
 # ebm <- str_trim(ebm, "both")
-ebm[str_detect(ebm, "^\\d{3,5}")]
+# ebm[str_detect(ebm, "^\\d{3,5}")]
 
 # Example EBM start in ebm[3098]
 
-ebm <- str_replace(ebm, pattern = "^\\s{9}", "")
-ebm <- ebm[str_detect(ebm, "^\\w")]
-ebm <- ebm[!(str_detect(ebm, "^(Stand)"))]
-ebm <- ebm[!(str_detect(ebm, "\\."))]
-ebm <- str_replace(ebm, "\\s\\w+\\,\\w+\\s$", "")
-ebm <- str_trim(ebm, "right")
 
-ebm_tab_pdf <- data.frame(ebm = str_extract(ebm, "^\\d+"),
-                          ebmtext = str_replace(ebm, "^\\d+", ""))
+ebm_to_table <- function(ebm_txt) {
+  ebm <- read_lines(ebm_txt, locale = locale(encoding = "ISO-8859-1"))
+  
+  ebm <- str_replace(ebm, pattern = "^\\s{9}", "")
+  ebm <- ebm[str_detect(ebm, "^\\w")]
+  ebm <- ebm[!(str_detect(ebm, "^(Stand)"))]
+  ebm <- ebm[!(str_detect(ebm, "\\."))]
+  ebm <- str_replace(ebm, "\\s\\w+\\,\\w+\\s$", "")
+  ebm <- str_trim(ebm, "right")
+  
+  ebm_tab <- data.frame(ebm = str_extract(ebm, "^\\d+"),
+                            ebmtext = str_replace(ebm, "^\\d+", ""))
+  
+  return(ebm_tab)
+}
+
+
